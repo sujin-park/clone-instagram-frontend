43474ae4532361a79ae9aad4d24a547706152f9c
Sujin92 <psujin831@gmail.com>
2019-09-12 15:07:14 +0900

    [A] add Router

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from '../Styles/Theme';
+import Router from './Router'
 
 export default () => (
   <ThemeProvider theme={Theme}>
+  <>
     <GlobalStyles />
-    <h1>Hello</h1>
+    <Router isLoggedIn={false}/>
+  </>
   </ThemeProvider>
 );
