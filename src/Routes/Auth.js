7fec843d512afe7291f27e68e34f2ca817037f42
sujin-park <psujin831@gmail.com>
2019-09-25 21:13:29 +0900

    [A] 5.2 Auth UI Part completed

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import Input from '../Components/Input';
+import Button from '../Components/Button';
 
 const Wrapper = styled.div`
   min-height: 80vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
@@ -11,7 +14,8 @@ const Wrapper = styled.div`
 const Box = styled.div`
   ${props => props.theme.whiteBox}
   border-radius:0px;
-  width: 350px;
+  width: 100%;
+  max-width: 350px;
 `;
 
 const StateChanger = styled(Box)`
@@ -24,24 +28,60 @@ const Link = styled.span`
   cursor: pointer;
 `;
 
+const Form = styled(Box)`
+  padding: 40px;
+  padding-bottom: 30px;
+  margin-bottom: 15px;
+  form {
+    width: 100%;
+    input {
+      width: 100%;
+      &:not(:last-child) {
+        margin-bottom: 7px;
+      }
+    }
+    button {
+      margin-top: 10px;
+    }
+  }
+`;
+
+
 export default () => {
   const [action, setAction] = useState("logIn");
   return (
     <Wrapper>
-      {action === "logIn" ? <Box>Login</Box> : <Box>Sign Up</Box>}
-      <StateChanger>
+      <Form>
         {action === "logIn" ? (
-          <>
-            Don't have an account?{" "}
-            <Link onClick={() => setAction("signUp")}>Sign up</Link>
-          </>
+            <form>
+                <Input placeholder={"Username"} />
+                <Input placeholder={"Password"} />
+                <Button text={"Log in"} />
+            </form>
         ) : (
-          <>
-            Have an account?{" "}
-            <Link onClick={() => setAction("logIn")}>Log in</Link>
-          </>
+            <form>
+                <Input placeholder={"First name"} />
+                <Input placeholder={"Last name"} />
+                <Input placeholder={"Email"} />
+                <Input placeholder={"Username"} />
+                <Input placeholder={"Password"} />
+                <Button text={"Sign up"} />
+            </form>
         )}
-      </StateChanger>
-    </Wrapper>
+    </Form>
+    <StateChanger>
+      {action === "logIn" ? (
+        <>
+          Don't have an account?{" "}
+          <Link onClick={() => setAction("signUp")}>Sign up</Link>
+        </>
+      ) : (
+        <>
+          Have an account?{" "}
+          <Link onClick={() => setAction("logIn")}>Log in</Link>
+        </>
+      )}
+    </StateChanger>
+  </Wrapper>
   );
 };
