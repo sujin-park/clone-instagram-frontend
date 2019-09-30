4adec9e09057e6f4fc90d5b3eb5f6bf809301a60
sujin-park <psujin831@gmail.com>
2019-09-30 22:30:25 +0900

    5.4 requestSecret Mutation and Refactor completed

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "../Routes/Auth";
+import Auth from "../Routes/Auth/index";
 import Feed from "../Routes/Feed";
 
 const LoggedInRoutes = () => (
---
+++ src/Routes/Auth/AuthContainer.js
@@ -0,0 +1,31 @@
+// Hooks, state
+import React, { useState } from 'react';
+import AuthPresenter from './AuthPresenter';
+import useInput from '../../Hooks/useInput';
+import { useMutation } from 'react-apollo-hooks';
+import { LOG_IN } from './AuthQueries';
+export default () => {
+    const [action, setAction] = useState("logIn");
+    const username = useInput("");
+    const firstName = useInput("");
+    const lastName = useInput("");
+    const email = useInput("");
+    const requestSecret = useMutation(LOG_IN, { variables: { email: email.value }});
+
+    const onLogin = (e) => {
+        e.preventDefault();
+        if (!email) {
+            requestSecret();
+        }
+    };
+
+    return <AuthPresenter
+        setAction={setAction}
+        action={action}
+        username={username}
+        firstName={firstName}
+        lastName={lastName}
+        email={email}
+        onLogin={onLogin}
+    />;
+};
